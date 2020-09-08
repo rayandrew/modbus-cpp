47346a2d188d1065a83cafe3333dcbc50c258f3f
Ray Andrew <raydreww@gmail.com>
2020-09-08 19:13:22 +0700

    enable shared libs

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -1,14 +1,13 @@
-# this file contains a list of tools that can be activated and downloaded on-demand each tool is
-# enabled during configuration by passing an additional `-DUSE_<TOOL>=<VALUE>` argument to CMake
-
 # only activate tools for top level project
 if(NOT PROJECT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
   return()
 endif()
 
-set(Boost_USE_STATIC_LIBS        ON)  # only find static libs
-set(Boost_USE_DEBUG_LIBS         OFF) # ignore debug libs and
-set(Boost_USE_RELEASE_LIBS       ON)  # only find release libs
+option(BOOST_STATIC "Boost static library" OFF)
+
+set(Boost_USE_STATIC_LIBS        ${BOOST_STATIC})  # only find static libs
+set(Boost_USE_DEBUG_LIBS         OFF)  # ignore debug libs and
+set(Boost_USE_RELEASE_LIBS       ON)   # only find release libs
 set(Boost_USE_MULTITHREADED      ON)
 set(Boost_USE_STATIC_RUNTIME     OFF)
 find_package(Boost 1.70.0 REQUIRED COMPONENTS
