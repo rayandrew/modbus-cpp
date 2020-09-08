92430d887ee2b27ab9d7c624a84eda6719fd3528
Ray Andrew <raydreww@gmail.com>
2020-09-09 01:27:34 +0700

    add flags in windows

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -11,3 +11,9 @@ set(Boost_USE_RELEASE_LIBS ON) # only find release libs
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
 find_package(Boost 1.70.0 REQUIRED COMPONENTS system)
+
+# attempt fixes from @al-cheb
+# https://github.com/actions/virtual-environments/issues/370#issuecomment-586209745
+if(WIN32)
+    add_definitions(-DBOOST_ALL_NO_LIB -DBOOST_PROGRAM_OPTIONS_DYN_LINK -DBOOST_IOSTREAMS_DYN_LINK -DBOOST_THREAD_DYN_LINK)
+endif()
